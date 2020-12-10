09580b11177bc0ce5188f86b652faa626268aca6
minhcc01-vn <72182255+minhcc01-vn@users.noreply.github.com>
2020-12-10 09:53:00 +0700

    Update

--- QuanLyDiem.sql
+++ QuanLyDiem.sql
@@ -1,12 +1,15 @@
-CREATE DATABASE QuanLyDiem2
-USE QuanLyDiem2
+
+CREATE DATABASE QuanLyDiem
+GO
+USE QuanLyDiem
+GO
 -- Cho cơ sở dữ liệu QuanLyDiem gồm các bảng dữ liệu sau:
 --SinhVien gồm các thuộc tính: masv varchar(5), holot nvarchar(30), ten nvarchar(10),
 -- gioitinh bit, ngaysinh datetime.
 -- Trong đó: masv là khóa chính.
 
 -- - MonHoc gồm các thuộc tính: mamh varchar(5), tenmh nvarchar(30), sotiet int
--- Trong đó: mamh là khóa chính; sotiet  > 0.
+-- Trong đó: mamh là khóa chính; sotiet  > 0.
 
 -- - DiemThi gồm các thuộc tính: masv varchar(5), mamh varchar(5), diemlan1 float, diemlan2 float.
 -- Trong đó: + masv, mamh là khóa chính
@@ -65,6 +68,8 @@ INSERT INTO SinhVien VALUES(@maSV, @hoLot, @ten, @gioiTinh, @ngaySinh)
 GO 
 EXEC sp_insert_sinhvien 'SV001','Enrdigo', 'Alasteir', 1, '2000-06-12'
 
+SELECT * FROM SinhVien
+
 ----------------Thủ tục sửa sinh viên -------------------------------------------
 GO
 CREATE PROC sp_update_sinhvien 
@@ -82,8 +87,10 @@ WHERE MaSV =  @maSV
 
 -- Test 
 GO 
-EXEC sp_update_sinhvien 'SV001', 'Curnucke', 'Tarra', 0, '2001-03-22'
 
+EXEC sp_update_sinhvien 'SV001', 'Curnucke', 'Tarra', 0, '2001-03-22'
+GO
+SELECT * FROM SinhVien
 
 ----------------- Thủ tục xóa sinh viên -------------------------
 GO
@@ -93,7 +100,7 @@ DELETE FROM SinhVien WHERE MaSV = @maSV
 -- Test
 GO
 sp_delete_sinhvien 'SV001'
-
+SELECT * FROM SinhVien
 ----------------------------------------------------------------
 -- 3. Tạo Function để đánh mã tự động cho bảng SinhVien.
 -----------------------------------------------------------------
@@ -112,6 +119,8 @@ GO
 
 -- Thêm dữ liệu bảng Sinh Viên
 INSERT INTO SinhVien VALUES (dbo.fc_auto_masv(), 'Hryncewicz', 'Arlie', 0, '2001-09-10')
+SELECT * FROM SinhVien
+
 INSERT INTO SinhVien VALUES (dbo.fc_auto_masv(), 'Bautiste', 'Brandyn', 0, '2001-03-12')
 INSERT INTO SinhVien VALUES (dbo.fc_auto_masv(), 'Tills', 'Harmony', 1, '2001-07-22')
 INSERT INTO SinhVien VALUES (dbo.fc_auto_masv(), 'Nussii', 'Antin', 1, '2001-08-12')
@@ -179,49 +188,6 @@ INSERT INTO DiemThi VALUES ('SV005', 'MH008', 8, 3)
 INSERT INTO DiemThi VALUES ('SV005', 'MH009', 8, 3)
 
 
-INSERT INTO DiemThi VALUES ('SV006', 'MH001', 1, 10)
-INSERT INTO DiemThi VALUES ('SV006', 'MH004', 4, 7)
-INSERT INTO DiemThi VALUES ('SV006', 'MH006', 6, 5)
-INSERT INTO DiemThi VALUES ('SV006', 'MH007', 7, 4)
-INSERT INTO DiemThi VALUES ('SV006', 'MH009', 9, 2)
-INSERT INTO DiemThi VALUES ('SV006', 'MH010', 10, 1)
-
-
-INSERT INTO DiemThi VALUES ('SV007', 'MH001', 1, 10)
-INSERT INTO DiemThi VALUES ('SV007', 'MH005', 5, 6)
-INSERT INTO DiemThi VALUES ('SV007', 'MH006', 6, 5)
-INSERT INTO DiemThi VALUES ('SV007', 'MH007', 7, 4)
-INSERT INTO DiemThi VALUES ('SV007', 'MH010', 8, 3)
-
-
-INSERT INTO DiemThi VALUES ('SV008', 'MH001', 1, 10)
-INSERT INTO DiemThi VALUES ('SV008', 'MH002', 2, 9)
-INSERT INTO DiemThi VALUES ('SV008', 'MH004', 4, 7)
-INSERT INTO DiemThi VALUES ('SV008', 'MH005', 5, 6)
-INSERT INTO DiemThi VALUES ('SV008', 'MH008', 8, 3)
-INSERT INTO DiemThi VALUES ('SV008', 'MH010', 10, 1)
-
-
-
-INSERT INTO DiemThi VALUES ('SV009', 'MH001', 4, 7)
-INSERT INTO DiemThi VALUES ('SV009', 'MH002', 2, 9)
-INSERT INTO DiemThi VALUES ('SV009', 'MH004', 4, 7)
-INSERT INTO DiemThi VALUES ('SV009', 'MH005', 5, 6)
-INSERT INTO DiemThi VALUES ('SV009', 'MH006', 6, 5)
-INSERT INTO DiemThi VALUES ('SV009', 'MH009', 9, 2)
-INSERT INTO DiemThi VALUES ('SV009', 'MH010', 10, 1)
-
-
-
-INSERT INTO DiemThi VALUES ('SV010', 'MH001', 1, 10)
-INSERT INTO DiemThi VALUES ('SV010', 'MH002', 4, 7)
-INSERT INTO DiemThi VALUES ('SV010', 'MH003', 3, 8)
-INSERT INTO DiemThi VALUES ('SV010', 'MH006', 6, 5)
-INSERT INTO DiemThi VALUES ('SV010', 'MH007', 7, 4)
-INSERT INTO DiemThi VALUES ('SV010', 'MH009', 9, 2)
-INSERT INTO DiemThi VALUES ('SV010', 'MH010', 10, 1)
-
-
 --------------------------------------------------------------------------------------------------
 -- 4. Tạo trigger kiểm tra tính hợp lệ về việc nhập điểm lần 2 cho các sinh viên. Tính hợp lệ được
 -- quy ước: Nếu điểm lần 1 < 5 mới cho phép nhập điểm lần 2.
@@ -232,11 +198,11 @@ ON DiemThi FOR INSERT
 AS
 BEGIN
     DECLARE @diemLan1 FLOAT
-    DECALRE @diemaLan2 FLOAT
+    DECLARE @diemLan2 FLOAT
     SELECT @diemLan1 = DiemLan1 FROM INSERTED
     SELECT @diemLan2 = DiemLan2 FROM INSERTED
     IF @diemLan1 > 5
-        IF @diemLan IS NOT NULL
+        IF @diemLan2 IS NOT NULL
             ROLLBACK TRANSACTION
 END
 
@@ -245,32 +211,31 @@ END
 -- lần 1 nhỏ hơn 4.
 SELECT sv.MaSV, sv.HoLot, sv.Ten, mh.TenMH, dt.DiemLan1 FROM SinhVien sv, MonHoc mh, DiemThi dt 
 WHERE sv.MaSV = dt.MaSV AND mh.MaMH = dt.MaMH 
-    AND mh.Ten = N'Cấu trúc dữ liệu' AND dt.DiemLan1 < 4
-
+    AND mh.TenMH = N'Cấu trúc dữ liệu' AND dt.DiemLan1 < 4
 
 -- 6. Sử dụng câu lệnh Select để hiển thị danh sách các sinh viên thi môn Cơ sở dữ liệu mà có
 -- điểm thi lần 1 >=5.
-SELECT sv.MSV, sv.HoLot, sv.Ten, mh.TenMH, dt.DiemLan1 FROM SinhVien sv, MonHoc mh, DiemThi dt
+SELECT sv.MaSV, sv.HoLot, sv.Ten, mh.TenMH, dt.DiemLan1 FROM SinhVien sv, MonHoc mh, DiemThi dt
 WHERE sv.MaSV = dt.MaSV AND mh.MaMH = dt.MaMH 
-    AND mh.Ten = N'Cơ sở dữ liệu' AND dt.DiemLan1 >= 5
+    AND mh.TenMH = N'Cơ sở dữ liệu' AND dt.DiemLan1 >= 5
 
 
--- 7. Sử dụng câu lệnh Select để hiển thị danh sách kết quả học tập của sinh viên gồm : Họ và tên,
+
+-- 7. Sử dụng câu lệnh Select để hiển thị danh sách kết quả học tập của sinh viên gồm : Họ và tên,
 -- Tên môn học, điểm thi cao nhất trong hai lần thi
 
 SELECT sv.HoLot, sv.Ten, mh.TenMH, dcn.DiemCaoNhat FROM SinhVien sv, MonHoc mh, 
 (SELECT dt.DiemLan1 AS DiemCaoNhat, dt.MaMH, dt.MaSV FROM DiemThi dt WHERE dt.DiemLan1 > dt.DiemLan2) dcn
 WHERE sv.MaSV = dcn.MaSV and dcn.MaMH = mh.MaMH
-UNION ALL
+UNION
 SELECT sv.HoLot, sv.Ten, mh.TenMH, dcn.DiemCaoNhat FROM SinhVien sv, MonHoc mh, 
 (SELECT dt.DiemLan2 AS DiemCaoNhat, dt.MaMH, dt.MaSV FROM DiemThi dt WHERE dt.DiemLan2 >= dt.DiemLan1) dcn
 WHERE sv.MaSV = dcn.MaSV and dcn.MaMH = mh.MaMH
 
 --------------------------------------------------------------------------------------------------------------
--- 8. Thống kê sinh viên có điểm thi lần 1 >=5, bao gồm các thông tin : Họ tên, Tên môn học, Tổng số sinh viên
+-- 8. Thống kê sinh viên có điểm thi lần 1 >=5, bao gồm các thông tin : Họ tên, Tên môn học, Tổng số sinh viên
 -------------------------------------------------------------------------------------------------------------- 
 SELECT mh.TenMH, TongSoSV = COUNT(sv.MaSV) FROM SinhVien sv, MonHoc mh, DiemThi dt 
 WHERE sv.MaSV = dt.MaSV and mh.MaMH = dt.MaMH
     AND dt.DiemLan1 >= 5
 GROUP BY mh.TenMH
-
